03c806afc743c7ecff8622581b48e2a3f8c233f0
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-10-05 12:55:09 +0300

    feat: done adaptive burger menu

--- museum-adaptive/src/assets/js/header.js
+++ museum-adaptive/src/assets/js/header.js
@@ -1,6 +1,29 @@
+const mediaQuery = window.matchMedia('(max-width: 420px)');
+const headerBackgroundImg1 = document.querySelector('.header__background-img1');
+const headerBackgroundImg2 = document.querySelector('.header__background-img2');
+const headerBackgroundImg3 = document.querySelector('.header__background-img3');
 const buttonBurgerMenu = document.querySelector('.header__burger-menu');
 const headerNav = document.querySelector('.header-nav');
+const headerBackground = document.querySelector('.header__container-background');
 const sectionWelcome = document.querySelector('.welcome');
+
+if (mediaQuery.matches) {
+  headerBackgroundImg1.setAttribute('src', './assets/img/design/header__1-420.jpg');
+  headerBackgroundImg2.setAttribute('src', './assets/img/design/header__2-420.jpg');
+  headerBackgroundImg3.setAttribute('src', './assets/img/design/header__3-420.jpg');
+}
+
+mediaQuery.addEventListener('change', (e) => {
+  if (mediaQuery.matches) {
+    headerBackgroundImg1.setAttribute('src', './assets/img/design/header__1-420.jpg');
+    headerBackgroundImg2.setAttribute('src', './assets/img/design/header__2-420.jpg');
+    headerBackgroundImg3.setAttribute('src', './assets/img/design/header__3-420.jpg');
+  } else {
+    headerBackgroundImg1.setAttribute('src', './assets/img/design/header__1-768.jpg');
+    headerBackgroundImg2.setAttribute('src', './assets/img/design/header__2-768.jpg');
+    headerBackgroundImg3.setAttribute('src', './assets/img/design/header__3-768.jpg');
+  }
+});
 
 const clickOutsideBurgerMenu = (e) => {
   if (buttonBurgerMenu.classList.contains('header__burger-menu--open')) {
@@ -18,11 +41,17 @@ const openCloseBurgerMenu = () => {
       buttonBurgerMenu.classList.remove('header__burger-menu');
       buttonBurgerMenu.classList.add('header__burger-menu--open');
       headerNav.style.left = '0';
+
+      headerBackground.style.left = '-20px';
+      
       sectionWelcome.classList.add('welcome--opacity');
     } else {
       buttonBurgerMenu.classList.remove('header__burger-menu--open');
       buttonBurgerMenu.classList.add('header__burger-menu');
-      headerNav.style.left = '-110%';
+      headerNav.style.left = '-150%';
+
+      headerBackground.style.left = '-150%';
+
       sectionWelcome.classList.remove('welcome--opacity');
     }
 };
