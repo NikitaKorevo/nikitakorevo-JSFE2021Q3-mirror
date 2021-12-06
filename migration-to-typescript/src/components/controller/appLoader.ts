7ff673b3e3b49ab6da100acbaa1bb7ff40958bae
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-12-07 00:44:31 +0300

    feat: change site in appLoader

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', { apiKey: 'https://nodenews.herokuapp.com/' }); // myApiKey: '2e3ea17d4abc4478b961b51e74ed5d77';
+    super('https://nodenews.herokuapp.com/', { apiKey: '2e3ea17d4abc4478b961b51e74ed5d77' }); // originalSite: 'https://newsapi.org/v2/'
   }
 }
 
